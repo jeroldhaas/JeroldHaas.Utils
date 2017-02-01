bb7666c9352dbbfca1779725438c4751ab118e29
Jerold Haas <jeroldhaas@gmail.com>
2017-02-01 09:49:57 -0500

    initial commit, tests passing

---
+++ EnumExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeroldHaas.Utils.Enums
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Returns the name of the enumeration, as a string.
+        /// </summary>
+        /// <param name="e">A "self"; generally you don't need to pass this,
+        /// as this is an extension method.</param>
+        /// <returns>The name of the enumeration, converted to string.</returns>
+        public static string Name(this Enum e)
+        {
+            var t = e.GetType();
+            return Enum.GetName(t, e);
+        }
+    }
+}
---
+++ JeroldHaas.Utils.Test/EnumExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using JeroldHaas.Utils.Enums;
+
+namespace JeroldHaas.Utils.Test
+{
+    [TestClass]
+    public class TestUtils
+    {
+        public enum Foo
+        {
+            One,
+            Two,
+            Three
+        }
+
+        [TestMethod]
+        public void NameOf_Returns_Name_Of_Enumeration()
+        {
+            var one = Foo.One;
+            var oneName = one.Name();
+
+            Assert.AreEqual("One", oneName, false, "Case Sensitive Test");
+            Assert.AreEqual("one", oneName, true, "Case Insensitive Test");
+        }
+    }
+}
